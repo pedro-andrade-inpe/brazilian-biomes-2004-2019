3cea04e95ba48574c59ed376ba79fb7719b40425
Pedro R. Andrade <pedro.andrade@inpe.br>
2023-09-14 07:56:21 -0300

    Adding legend to each map

--- 5-plot-biomes.R
+++ 5-plot-biomes.R
@@ -16,36 +16,40 @@ biomes2019$biome <- biomes
 
 colors = c("#b8fd00", "#fffdae", "#fdc5b0", "#d9fbab", "#fceed3", "#fee7ff")
 
-orig2004 <- 
-  tmap::tm_shape(origbiomes2004) +
+origbiomes2019$Biomes <- origbiomes2019$biome
+biomes2019$Biomes <- biomes2019$biome
+
+orig2004 <- tmap::tm_shape(origbiomes2004) +
   tmap::tm_polygons("biome", palette = colors) +
-  tmap::tm_layout(legend.show = FALSE) +
-  tmap::tm_credits("Biomes in 2004\nas defined by IBGE", position = "left", align = "left")
+  tmap::tm_credits("Biomes in 2004\nas defined by IBGE", size = 1.4, position = "left", align = "left") +
+  tm_legend(
+    scale = 0.4,
+    legend.show = FALSE)
 
 orig2019 <- tmap::tm_shape(origbiomes2019) +
-  tmap::tm_polygons("biome", palette = colors) +
-  tmap::tm_layout(legend.show = FALSE) +
-  tmap::tm_credits("Biomes in 2019\nas defined by IBGE", position = "left", align = "left")
+  tmap::tm_polygons("Biomes", palette = colors) +
+  tmap::tm_credits("Biomes in 2019\nas defined by IBGE", size = 1.4, position = "left", align = "left") +
+  tm_legend(
+    scale = 0.4,
+    title.size = 1.5,
+    text.size = 1.1,
+    position = c("right","bottom"))
 
 final2004 <- tmap::tm_shape(biomes2004) +
   tmap::tm_polygons("biome", palette = colors) +
-  tmap::tm_layout(legend.show = FALSE) +
-  tmap::tm_credits("Biomes in 2004\nafter processing", position = "left", align = "left")
+  tmap::tm_credits("Biomes in 2004\nafter processing", size = 1.4, position = "left", align = "left") + tm_legend(#legend.only = TRUE, 
+    scale = 0.4,
+    show = FALSE)
 
-final2019 <- tmap::tm_shape(biomes2019) +
-  tmap::tm_polygons("biome", palette = colors) +
-  tmap::tm_layout(legend.show = FALSE) +
-  tmap::tm_credits("Biomes in 2019\nafter processing", position = "left", align = "left")
-
-origbiomes2019$Biomes <- origbiomes2019$biome
-
-legend.map <- 
-  tmap::tm_shape(origbiomes2019) +
+final2019 <-  
+  tmap::tm_shape(biomes2019) +
   tmap::tm_polygons("Biomes", palette = colors) +
-  tm_layout(legend.only = TRUE, 
-            legend.title.size = 1.5, frame.double.line = TRUE,
-            legend.text.size = 1.1,
-            legend.position = c("left","top"))
+  tmap::tm_credits("Biomes in 2019\nafter processing", size = 1.4, position = "left", align = "left") +
+  tm_legend(
+          scale = 0.4,
+          title.size = 1.5,
+          text.size = 1.1,
+          position = c("right","bottom"))
 
 pdf("biomes-orig.pdf", width = 6, height = 3)
 grid::grid.newpage()
@@ -56,15 +60,11 @@ print(orig2004, vp=grid::viewport(layout.pos.row = 1, layout.pos.col = 1))
 print(orig2019, vp=grid::viewport(layout.pos.row = 1, layout.pos.col = 2))
 dev.off()
 
-pdf("biomes-final.pdf", width = 6, height = 3)
+pdf("biomes-final2.pdf", width = 6, height = 3)
 grid::grid.newpage()
 page.layout <- grid::grid.layout(nrow = 1, ncol = 2)
 grid::pushViewport(grid::viewport(layout = page.layout))
 
 print(final2004, vp=grid::viewport(layout.pos.row = 1, layout.pos.col = 1))
 print(final2019, vp=grid::viewport(layout.pos.row = 1, layout.pos.col = 2))
-dev.off()
-
-pdf("biomes-legend.pdf", width = 2, height = 3)
-print(legend.map)
 dev.off()
