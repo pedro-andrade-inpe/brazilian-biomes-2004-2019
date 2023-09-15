cda812762892fa1224758df3da52124eebd625d6
Pedro R. Andrade <pedro.andrade@inpe.br>
2023-09-15 15:40:43 -0300

    New version of figure 3

--- 6-plot-changes-biomes-states.R
+++ 6-plot-changes-biomes-states.R
@@ -7,11 +7,21 @@ states <- geobr::read_state() %>%
 intersec <- sf::read_sf("results/biomes-intersec.gpkg")
 intersec <- intersec[-c(1, 4, 9, 14, 17, 20), ] # remove the intersections with the biome itself
 
-RColorBrewer::brewer.pal(7, "Set3")
+to <- intersec %>%
+  dplyr::group_by(code_biome.1) %>%
+  dplyr::summarize()
 
-pdf("changes-biomes-states.pdf")
+colors = c("#b8fd00", "#fffdae", "#fdc5b0", "#d9fbab", "#fceed3", "#fee7ff")
+to["New Biome"] <- c("Amazonia", "Caatinga", "Cerrado", "Mata Atlantica", "Pampa", "Pantanal")
+
+pdf("changes-biomes-states.pdf", width = 3, height = 3)
 tmap::tm_shape(states) +
   tmap::tm_polygons(col = "#80B1D3") +
-  tmap::tm_shape(intersec) +
-  tmap::tm_polygons(col = "orange", lwd=0)
+  tmap::tm_shape(to) +
+  tmap::tm_polygons("New Biome", palette = colors, lwd=0) +
+  tmap::tm_legend(
+    scale = 0.4,
+    title.size = 1.5,
+    text.size = 1.1,
+    position = c("right","bottom"))
 dev.off()
